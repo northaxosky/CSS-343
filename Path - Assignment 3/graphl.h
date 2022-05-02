8d6bd14211694a95e264ea17788af88da8a98b12
Mute <legendgm@outlook.com>
2022-05-01 19:32:30 -0700

    .h

---
+++ Path - Assignment 3/graphl.h
@@ -0,0 +1,38 @@
+#ifndef GRAPHL_H
+#define GRAPHL_H
+#include "nodedata.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+const int MAXNODES = 100;
+
+struct EdgeNode {
+    int adjGraphNode;
+    EdgeNode* nextEdge;
+};
+
+struct GraphNode    {
+    EdgeNode* edgeHead;
+    NodeData* data;
+    bool visited;
+};
+
+class GraphL    {
+public:
+    GraphL();
+    ~GraphL();
+
+    void buildGraph(ifstream& file);
+    void depthFirstSearch();
+    void displayGraph();
+
+private:
+    int size;
+    GraphNode nodes[MAXNODES];
+
+    void empty();
+    void DFSHelper(int n);
+};
+
+#endif
