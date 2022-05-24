30834e20ef9537c3964f7f6a5d27bb99330a61e6
Mute <legendgm@outlook.com>
2022-05-24 16:17:04 -0700

    design & shop

---
+++ Inventory - Assignment 4/shop.h
@@ -0,0 +1,14 @@
+#ifndef SHOP_ASS_4
+#define SHOP_ASS_4
+#include <iostream>
+#include <fstream>
+#include "movie.h"
+#include "customer.h"
+#include "inventory.h"
+using namespace std;
+
+class Shop  {
+
+};
+
+#endif
